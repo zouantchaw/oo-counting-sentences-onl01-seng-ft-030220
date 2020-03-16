35ac972995586aab338f5a8be4573da8014c8ec1
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-16 07:01:43 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
 
   def count_sentences
-    self.split(".")
-    binding.pry 
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
